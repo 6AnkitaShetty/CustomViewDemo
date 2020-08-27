e0f62d6333b95208b2d3e5f5e44b8c119af555ad
Ankita <avshetty.6@gmail.com>
2020-08-27 11:48:28 +0200

    Custom View Example1

---
+++ app/src/main/java/com/example/customviewdemo/NoteKeeperData.kt
@@ -0,0 +1,16 @@
+package com.example.customviewdemo
+
+import android.graphics.Color
+
+data class CourseInfo(val courseId: String, val title: String) {
+    override fun toString(): String {
+        return title
+    }
+}
+
+data class NoteInfo(
+    var course: CourseInfo? = null,
+    var title: String? = null,
+    var text: String? = null,
+    var color: Int = Color.TRANSPARENT
+)
